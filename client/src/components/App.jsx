e095dc668abbaea060e0ebc818773a43078eb0ed
Kevin Lam <klam@Kevins-Macbook-Pro.local>
2019-07-13 19:27:18 -0700

    Adds simple list, and connects to ListItem

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 
+import ListItem from './ListItem.jsx';
+
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			test: 'hi'
+			list: ['Ford', 'Chevrolet', 'Mazda', 'Toyota', 'Honda', 'Dodge', 'BMW', 'Mercedes']
 		}
 	}
 
 	render() {
-		return(<div>{this.state.test}</div>);
+		return(
+			<div>
+				<h1>My Favorite Car Brands</h1>
+				<ol>
+					{this.state.list.map((car, i) => {
+						return <ListItem car={car} key={i} />
+					})}
+				</ol>
+			</div>
+		);
 	}
 };
